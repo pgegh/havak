13782be288076fd6020799e41b201f7f2f3293a9
Hovig Manjikian <hovig@mail.com>
2025-01-04 19:50:06 +0100

    Refactor implementation

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,54 @@
+use env_logger;
 use libc;
 use log::{error, info};
-use std::fs::File;
-use std::io::Write;
-use std::thread;
-use std::time::Duration;
 
-fn daemonize() -> std::io::Result<()> {
-    //Fork the process
-    let pid = unsafe { libc::fork() };
-    if pid < 0 {
-        // Fork failed
-        return Err(std::io::Error::new(
-            std::io::ErrorKind::Other,
-            "Fork failed",
-        ));
-    }
-    else if pid > 0 {
-        // Parent process exits, leaving the child to run as a daemon
-        std::process::exit(0);
-    }
-    else {
-        // Child process continues here.
-        // Create a new session and become the session leader.
-        let pgid = unsafe { libc::setsid() };
-        if pgid < 0 {
-            // Setsid failed
-            return Err(std::io::Error::new(
-                std::io::ErrorKind::Other,
-                "Setsid failed",
-            ));
-        }
-
-        // Redirect stdout and stderr to a log file
-        let log_file = File::create("temp/daemon.log")?;
-        let mut log_writer = std::io::BufWriter::new(log_file);
-
-        // Example of some long-running work the daemon does
-        loop {
-            writeln!(
-                log_writer,
-                "Daemon running at: {:?}",
-                std::time::SystemTime::now()
-            )
-                .unwrap();
-            log_writer.flush().unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-    }
-}
+pub mod server;
 
 fn main() {
-    // Initialize logger
     env_logger::init();
 
     match daemonize() {
         Ok(_) => info!("Daemon started successfully."),
         Err(e) => error!("Failed to start daemon: {}", e),
+    }
+
+    match crate::server::start_sever() {
+        Ok(_) => info!("Server executed successfully."),
+        Err(e) => error!("Failed to execute the server correctly: {}", e),
+    }
+}
+
+fn daemonize() -> std::io::Result<()> {
+    fork_the_process()?;
+    execute_system_call_setsid()
+}
+
+fn fork_the_process() -> std::io::Result<()> {
+    let pid = unsafe { libc::fork() };
+    if pid < 0 {
+        // Fork failed
+        Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "Fork failed",
+        ))
+    } else if pid > 0 {
+        // Parent process exits, leaving the child to run as a daemon
+        std::process::exit(0)
+    } else {
+        // Child process continues here.
+        Ok(())
+    }
+}
+
+fn execute_system_call_setsid() -> std::io::Result<()> {
+    let sid = unsafe { libc::setsid() };
+    if sid < 0 {
+        // Setsid failed
+        Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "Setsid failed",
+        ))
+    } else {
+        Ok(())
     }
 }
---
+++ src/server/mod.rs
@@ -0,0 +1,21 @@
+use std::fs::File;
+use std::io::Write;
+use std::thread;
+use std::time::Duration;
+
+pub fn start_sever() -> std::io::Result<()> {
+    // Redirect stdout and stderr to a log file
+    let log_file = File::create("temp/daemon.log")?;
+    let mut log_writer = std::io::BufWriter::new(log_file);
+
+    // Example of some long-running work the daemon does
+    loop {
+        writeln!(
+            log_writer,
+            "Daemon running at: {:?}",
+            std::time::SystemTime::now()
+        )?;
+        log_writer.flush()?;
+        thread::sleep(Duration::from_secs(1));
+    }
+}
